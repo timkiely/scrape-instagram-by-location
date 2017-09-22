7332c774ecf272e192fcf21f4ebca49554cfdbff
timkiely <timothy.j.kiely@gmail.com>
2017-09-22 12:07:00 -0400

    updated network test with network error handling

--- search-for-location-ids.R
+++ search-for-location-ids.R
@@ -23,7 +23,18 @@ srch <- search_for_location_ids(location = "Williamsburg, New York")
 
 
 
+# testing stuff -----------------------------------------------------------
+meters_per_mile <- 100
+lat <- 40.716757 
+lon <- -73.959584
 
 
+'https://api.instagram.com/v1/locations/search?lat=48.858844&lng=2.294351'
 
-
+rm(response)
+qry <- "https://www.instagram.com/web/search/topsearch/?context=place&lat=%s&lng=%s"
+qry_full <- sprintf(qry, lat, lon)
+req <- GET(qry_full)
+(txt <- content(req, "text", encoding = "UTF-8") )
+response <- jsonlite::fromJSON(txt)
+response
